a87ed854c75c584ffbd644f59b929effa80a6b09
Benoît - Xavier HOUVET <benoit-xavier.houvet@lydia-app.com>
2019-05-15 13:35:07 +0200

    test

--- ci-wcs.sh
+++ ci-wcs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-branchName=$1
+branchNameBase=$1
+branchNameTarget=$2
 
 if [ $branchName ]
 then
     echo "Vous etes sur la branch :"
     echo $branchName
+    echo "Voys voulez pousser sur la branch :"
+    echo $branchNameTarget
 fi
 
