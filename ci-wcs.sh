81b6597825207f6eb626891571f658e7ec9543ca
Benoît - Xavier HOUVET <benoit-xavier.houvet@lydia-app.com>
2019-05-15 13:43:28 +0200

    test

--- ci-wcs.sh
+++ ci-wcs.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 branchNameBase=$1
-branchNameTarget=$2
+CIRCLE_PR_NUMBER=$2
+GITHUB_TOKEN="921537988b09af69e70cdc1d9e03bc44a83a3c67"
 
-if [ $branchName ]
+if [ $branchNameBase ]
 then
     echo "Vous etes sur la branch :"
-    echo $branchName
-    echo "Voys voulez pousser sur la branch :"
-    echo $branchNameTarget
+    echo $branchNameBase
+   # echo "Voys voulez pousser sur la branch :"
+   # echo $branchNameTarget
 fi
 
+if [[ -n ${CIRCLE_PR_NUMBER} ]]
+then
+    curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
+      -o jq
+    chmod +x jq
+    url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
+    target_branch=$(
+      curl "$url" | ./jq '.base.ref' | tr -d '"'
+    )
+fi
+
+echo $target_branch
