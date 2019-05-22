8fe03c4c08a20e3c6c50d171d4f64776f5e62d20
Benoît - Xavier HOUVET <benoit-xavier.houvet@lydia-app.com>
2019-05-22 14:04:26 +0200

    test

--- ci-wcs.sh
+++ ci-wcs.sh
@@ -14,6 +14,6 @@ fi
 prefix=$(echo $branchNameBase | tr "/" "\n")
 
 echo "Prefix :"
-echo "${prefix[0]}";
+echo ${prefix[0]};
 
 #if (($prefix[0] === "feat" || $prefix[0] === "fix") && $branchDestination === "dev")
