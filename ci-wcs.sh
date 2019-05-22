c7c2fa11fb8de4b962a7db9a15ec1560021179fa
Benoît - Xavier HOUVET <benoit-xavier.houvet@lydia-app.com>
2019-05-22 11:26:34 +0200

    Update for travis

--- ci-wcs.sh
+++ ci-wcs.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
+branchNameBase = $TRAVIS_PULL_REQUEST_BRANCH
+branchDestination = $TRAVIS_BRANCH
+
 if [ $branchNameBase ]
 then
     echo "Vous etes sur la branch :"
     echo $branchNameBase
-   # echo "Voys voulez pousser sur la branch :"
-   # echo $branchNameTarget
+    echo "Voys voulez pousser sur la branch :"
+    echo $branchDestination
 fi
-
-if [ $CIRCLE_PR_NUMBER ]
-then
-    curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
-      -o jq
-    chmod +x jq
-    url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
-    target_branch=$(
-      curl "$url" | ./jq '.base.ref' | tr -d '"'
-    )
-fi
-
-echo $target_branch
