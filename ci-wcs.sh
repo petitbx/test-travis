78b6c3f55048842eb4e873c7b8d45259c882abd0
Benoît - Xavier HOUVET <benoit-xavier.houvet@lydia-app.com>
2019-05-23 11:00:36 +0200

    test again

--- ci-wcs.sh
+++ ci-wcs.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 BRANCHNAMEBASE=$TRAVIS_PULL_REQUEST_BRANCH
-BRANCHDESTINATION=$TRAVIS_BRANCH
+BRANCHNAMEBASE="feat/circle"
 
+BRANCHDESTINATION=$TRAVIS_BRANCH
+BRANCHDESTINATION="dev"
 if [ $BRANCHNAMEBASE ]
 then
     echo "You're on branch :"
@@ -13,9 +15,32 @@ fi
 
 IFS='/' read -ra PREFIX <<< "$BRANCHNAMEBASE"
 
+if [ ${PREFIX[0]} != "feat" ] && [ ${PREFIX[0]} != "fix" ] && [ ${PREFIX[0]} != "hotfix" ] ; then
+  echo "Bad naming of branch !!!";
+  echo "As a reminder, your branch must start with the prefix 'feat' or 'fix' or 'hotfix' !!!!!!";
+  exit 1;
+fi
+
 if [ ${PREFIX[0]} = "feat" ] || [ ${PREFIX[0]} = "fix" ] ; then
   if [ $BRANCHDESTINATION != "dev" ] ; then
-    echo "Impossible de pousser sur la branch ${BRANCHDESTINATION} avec le prefix ${PREFIX[0]}. Uniquement dev";
+    echo "Not allowed to push on branch ${BRANCHDESTINATION} with prefix ${PREFIX[0]}. Only dev";
     exit 1;
   fi
 fi
+
+if [ ${PREFIX[0]} = "hotfix" ] ; then
+  if [ $BRANCHDESTINATION != "master" ] ; then
+    echo "Not allowed to push on branch ${BRANCHDESTINATION} with prefix ${PREFIX[0]}. Only master";
+    exit 1;
+  fi
+fi
+
+FILES=$(git diff --name-only)
+NUMBERFILES=${#FILES[@]}
+
+if [ $NUMBERFILES -gt 10 ] ; then
+  echo "Your PR contains too many files. You have $NUMBERFILES files.";
+  echo "As a reminder, you must have 10 files max.";
+  exit 1;
+fi
+
